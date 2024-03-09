e01db0c1a1010bf28acff04b9df3a46a61fd7a9e
adeeshaKSoftcodeit <adeeshaKSoftcodeit>
2024-03-09 23:12:57 +0530

    DB connection with User REST init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'qwerty123',
+      database: 'digitalhippo',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      legacySpatialSupport: false,
+      keepConnectionAlive: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
